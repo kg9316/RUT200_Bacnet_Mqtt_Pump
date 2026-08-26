00ffcedca90f3c5933a9f5ccffacbc17538c9fe5
Kato Gangstad <kato.gangstad@gmail.com>
2026-08-27 00:16:44 +0200

    Refactor gateway into modules: device table API

---
+++ src/device_table.h
@@ -0,0 +1,11 @@
+#ifndef RUT200_DEVICE_TABLE_H
+#define RUT200_DEVICE_TABLE_H
+
+#include "gateway.h"
+
+DEVICE_STATE *find_device(uint32_t id);
+DEVICE_STATE *get_or_create_device(uint32_t id);
+bool object_type_has_present_value(BACNET_OBJECT_TYPE type);
+POINT_STATE *add_point(DEVICE_STATE *device, BACNET_OBJECT_TYPE type, uint32_t instance);
+
+#endif

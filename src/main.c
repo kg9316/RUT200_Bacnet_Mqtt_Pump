6dc6194a447017d41eb51085b1c23cb2ba75b40b
Kato Gangstad <kato.gangstad@gmail.com>
2026-08-27 02:24:39 +0200

    Add syslog and runtime status loop

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include "bacnet_client.h"
 #include "gateway.h"
+#include "logger.h"
 #include "mqtt_client.h"
+#include "status.h"
 
 #include <signal.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 static volatile sig_atomic_t g_running = 1;
 
@@ -27,35 +29,52 @@ int main(int argc, char **argv)
         g_mqtt_port = atoi(argv[3]);
     if (argc > 4)
         safe_copy(g_topic_root, sizeof(g_topic_root), argv[4]);
+    if (argc > 5)
+        g_poll_ms = (unsigned)strtoul(argv[5], NULL, 10);
+    if (argc > 6)
+        g_discovery_ms = (unsigned)strtoul(argv[6], NULL, 10);
+    if (argc > 7)
+        g_max_age_sec = (unsigned)strtoul(argv[7], NULL, 10);
 
     signal(SIGINT, signal_handler);
     signal(SIGTERM, signal_handler);
 
-    printf("GK BACnet -> MQTT Gateway\n");
-    printf("BACnet=%s MQTT=%s:%d root=%s\n",
-           bacnet_interface,
-           g_mqtt_host,
-           g_mqtt_port,
-           g_topic_root);
+    LOG_OPEN();
+    LOG_INFOF("starting BACnet=%s MQTT=%s:%d root=%s poll=%ums discovery=%ums maxAge=%us",
+              bacnet_interface,
+              g_mqtt_host,
+              g_mqtt_port,
+              g_topic_root,
+              g_poll_ms,
+              g_discovery_ms,
+              g_max_age_sec);
 
     rc = mqtt_client_init();
     if (rc != 0) {
-        fprintf(stderr, "MQTT initialization failed rc=%d\n", rc);
+        LOG_ERRORF("MQTT initialization failed rc=%d", rc);
+        LOG_CLOSE();
         return 1;
     }
 
     if (bacnet_client_init(bacnet_interface) != 0) {
-        fprintf(stderr, "datalink_init failed\n");
+        LOG_ERRORF("BACnet datalink initialization failed interface=%s", bacnet_interface);
         mqtt_client_cleanup();
+        LOG_CLOSE();
         return 1;
     }
+
+    status_write_now();
 
     while (g_running) {
         mqtt_client_loop();
         bacnet_client_loop();
+        status_write_if_due();
     }
 
+    LOG_INFOF("stopping");
     bacnet_client_cleanup();
     mqtt_client_cleanup();
+    unlink("/tmp/gk-bacnet-mqtt-status.json");
+    LOG_CLOSE();
     return 0;
 }
